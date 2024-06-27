f881398a8cc662ee6294b6bcd6bf4f2086640b83
tseng91301 <tseng91301@gmail.com>
2024-06-27 08:59:47 +0800

    string_example.cpp

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1 = "abc";
+    string s2;
+    cout<<"Input s2: ";
+    cin>>s2;
+    string s3 = s1 + s2;
+    cout<<s3<<endl;
+    cout<<s3[3]<<endl;
+}
