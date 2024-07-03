d784b0fb81579366496de62b64f92325bc7ca3d6
tseng91301 <tseng91301@gmail.com>
2024-07-03 14:39:27 +0800

    aa

---
+++ example/char_calculate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c1 = 'A';
+    char c2 = ' ';
+    cout<<"c1: "<<c1<<endl; // A
+    cout<<"c2: "<<c2<<endl; // 空格
+    cout<<"(c1 + 1) = "<<(c1 + 1)<<endl; // 66
+    cout<<"(c1 + c2) = "<<(c1 + c2)<<endl; // 97
+    cout<<"('a'/2) = "<<('a'/2)<<endl; // 48
+}
